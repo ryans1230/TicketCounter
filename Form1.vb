d95447f0556b814ce8cc1f9cc21848e317856283
Ryan <ryans1230@gmail.com>
2019-10-22 13:02:50 -0400

    Initial commit

---
+++ Form1.vb
@@ -0,0 +1,28 @@
+﻿Public Class Form1
+    Private todayTickets As Int16 = 0
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim fileReader As String
+        fileReader = My.Computer.FileSystem.ReadAllText("ticketCounter.txt")
+        Label2.Text = fileReader
+    End Sub
+
+    Private Sub IncrementButton_Click(sender As Object, e As EventArgs) Handles IncrementButton.Click
+        Dim number = Convert.ToInt32(Label2.Text)
+        number += 1
+        todayTickets += 1
+        Label2.Text = number.ToString
+        TodayCounter.Text = todayTickets.ToString
+
+        My.Computer.FileSystem.WriteAllText("ticketCounter.txt", Label2.Text, False)
+    End Sub
+
+    Private Sub OopsButton_Click(sender As Object, e As EventArgs) Handles OopsButton.Click
+        Dim number = Convert.ToInt32(Label2.Text)
+        number -= 1
+        todayTickets -= 1
+        Label2.Text = number.ToString
+        TodayCounter.Text = todayTickets.ToString
+
+        My.Computer.FileSystem.WriteAllText("ticketCounter.txt", Label2.Text, False)
+    End Sub
+End Class
